9df2aacba63d705c9a8106881b3295908209abff
Afrid Ahmed <afridaskar@gmail.com>
2026-09-03 01:28:13 +0530

    feat: add main menu & while loop

--- src/main.c
+++ src/main.c
@@ -26,3 +26,29 @@ void print_banner(){
     printf("\n");
 
 }
+
+void print_menu(){
+    //Main menu
+    printf("1. Start Simulation - S");
+    printf("2. View Instructions - I");
+    printf("Simulation Statics - T");
+    printf("Exit - X");
+
+    printf("> ");
+}
+
+
+int main(){
+    int is_running = 1;
+    char choice;
+
+    while(is_running){
+        print_banner();
+        print_menu();
+        
+        scanf(" %c", &choice);//Get user's choice
+        
+
+
+    return 0;
+}
