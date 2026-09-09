c57c6b6fb3364062954ed759b5e3646fd093eec0
Afrid Ahmed <afridaskar@gmail.com>
2026-09-09 11:17:41 +0530

    feat: add part1c initial

--- src/part1a.c
+++ src/part1a.c
@@ -6,7 +6,7 @@
 #define GRAVITY 9.81
 
 // Helper function to calculate distance between two coordinates
-double calculate_distance(double x1, double y1, double x2, double y2) {
+static double calculate_distance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
