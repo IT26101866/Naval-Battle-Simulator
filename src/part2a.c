993a9af8383a94ffea866a58a3a6a2f2fe04ab68
Afrid Ahmed <afridaskar@gmail.com>
2026-09-09 16:04:14 +0530

    feat: add random k points & initial gun jam

--- src/part2a.c
+++ src/part2a.c
@@ -33,4 +33,21 @@ void run_part2a(Battlefield *field) {
     double battleship_damage = 0.0; 
     int total_hits_by_battleship = 0;
     double total_battle_time = 0.0;
+
+    for (int step = 1; step <= field->k_points; step++)
+    {
+        printf("\n--- Step %d/%d ---\n", step, field->k_points);
+
+        field->player_ship.x_pos = ((double)rand() / RAND_MAX) * field->canvas_size;
+        field->player_ship.y_pos = ((double)rand() / RAND_MAX) * field->canvas_size;
+
+        printf("Battleship moved to coordinates: (%.2f, %.2f)\n", field->player_ship.x_pos, field->player_ship.y_pos);
+
+        int is_jammed = 0;
+        if (step > field->jam_step) {
+            is_jammed = 1;
+            printf("[WARNING] Gun jammed! Restricted to %.2f degrees.\n", field->theta_min);
+        }
+    }
+    
 }
