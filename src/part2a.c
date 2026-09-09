8dca5993df88df37538b83b79e038a01585eead8
Afrid Ahmed <afridaskar@gmail.com>
2026-09-09 15:57:44 +0530

    feat: add compare threat function

--- src/part2a.c
+++ src/part2a.c
@@ -17,3 +17,11 @@ static double calculate_distance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
 
+// Comparison function for qsort (sorts descending by threat score)
+static int compare_threats(const void *a, const void *b) {
+    TargetThreat *t1 = (TargetThreat *)a;
+    TargetThreat *t2 = (TargetThreat *)b;
+    if (t1->threat_score < t2->threat_score) return 1;
+    if (t1->threat_score > t2->threat_score) return -1;
+    return 0;
+}
