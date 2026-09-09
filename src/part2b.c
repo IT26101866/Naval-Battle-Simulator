1dae9b843b098e8992aa2f2f194d31fdfbd10af3
Afrid Ahmed <afridaskar@gmail.com>
2026-09-09 18:04:05 +0530

    feat: add par2b and initial features

---
+++ src/part2b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../include/simulator.h"
+
+#define GRAVITY 9.81
+
+typedef struct {
+    int array_index;
+    int ship_id;
+    double distance;
+    double threat_score;
+} TargetThreat;
+
+static double calculate_distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+static int compare_threats(const void *a, const void *b) {
+    TargetThreat *t1 = (TargetThreat *)a;
+    TargetThreat *t2 = (TargetThreat *)b;
+    if (t1->threat_score < t2->threat_score) return 1;
+    if (t1->threat_score > t2->threat_score) return -1;
+    return 0;
+}
+
